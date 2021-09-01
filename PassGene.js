f1b3baa689d03b1affaea37def7e132f508d77f4
Mohammed Ellouke <50714453+MohammedEllk@users.noreply.github.com>
2021-09-01 17:55:15 +0100

    fix success msg + fix update pass
    fix success msg + fix update pass

--- PassGene.js
+++ PassGene.js
@@ -1,5 +1,8 @@
+
 function generate(e){
 e.preventDefault();
+document.getElementById("passhash").innerHTML = "";
+console.log("test1");
 var lengthPass = Number(document.getElementById('passlen').value);
 var majes = document.getElementsByName('majes');
 var symb = document.getElementsByName('symb');
@@ -18,58 +21,115 @@ var isNumb = false;
   if (numb[0].checked) {
     isNumb= true;
   }
-
+  document.getElementById("passhash").innerHTML = htmlEncode(Afunctionthatdosomething(lengthPass,isSymb,isNumb,isMajes));
+  var textpass = document.getElementById("passhash").innerHTML;
+  document.getElementById("lengtext").innerHTML = Afunctionthatdosomething(lengthPass,isSymb,isNumb,isMajes).length;
+  ChangeTexte(textpass);
 
 console.log(isMajes);
 console.log(lengthPass);
 console.log(isSymb);
 console.log(isNumb);
-document.getElementById("passhash").innerHTML = htmlEncode(Afunctionthatdosomething(lengthPass,isSymb,isNumb,isMajes));
 
+
+}
+function ChangeTexte(textpass,lengthPass,isSymb,isNumb,isMajes){
+	
+	console.log(document.getElementById("passhash").innerHTML);
+	$('#passhash').val(textpass);
+	console.log(document.getElementById("passhash").innerHTML);
+	
+	console.log("test 2");
 
 }
 function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
+	console.log("test 3");
+	var textArea = document.createElement("textarea");
+	textArea.value = text;
   
-  // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
+	// Avoid scrolling to bottom
+	textArea.style.top = "0";
+	textArea.style.left = "0";
+	textArea.style.position = "fixed";
 
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
 
-  try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
-  }
-
-  document.body.removeChild(textArea);
+	try {
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+		console.log('Fallback: Copying text command was ' + msg);
+	} catch (err) {
+		console.error('Fallback: Oops, unable to copy', err);
+	}
+	console.log("test 4");
+	document.body.removeChild(textArea);
 }
 function copyTextToClipboard(text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text);
-    return;
+	console.log("test 5");
+	if (!navigator.clipboard) {
+		fallbackCopyTextToClipboard(text);
+		console.log("7midaaa");
+		return;
   }
-  navigator.clipboard.writeText(text).then(function() {
-    console.log('Async: Copying to clipboard was successful!');
-  }, function(err) {
-    console.error('Async: Could not copy text: ', err);
+	console.log("test 6");
+	navigator.clipboard.writeText(text).then(function() {
+	console.log("test 7mida");
+	setTimeout(showsuccess, 100);
+	setTimeout(hidesuccess, 1500);
+
+	
+	}, function(err) {
+		console.error('Async: Could not copy text: ', err);
   });
+}
+function Resolve(function1,time) {
+  
+   return new Promise(resolve => {
+    setTimeout(function1, time);
+  });
+
+}
+function StopSucc(func) {
+  
+   return new Promise(resolve => {
+    clearTimeout(func);
+  });
+
 }
 
 var copyPassword = document.querySelector('.js-copy-password-btn');
-  
-
 copyPassword.addEventListener('click', function(event) {
-	console.log(document.getElementById("passhash").textContent.length);
-	copyTextToClipboard(document.getElementById("passhash").textContent);
+console.log(document.getElementById("passhash").textContent.length);
+copyTextToClipboard(document.getElementById("passhash").textContent);
 });
+
+function showsuccess(){
+	document.getElementById("success").style.visibility = "visible";
+	document.getElementById("success").style.width =  "400px";
+	document.getElementById("success").style.height =  "30px";
+	document.getElementById("success").style.backgroundColor = "green";
+	document.getElementById("success").innerHTML = "Password copied successfully!! sa longeur est "+document.getElementById("passhash").textContent.length; 
+}
+function hidesuccess(){
+	document.getElementById("success").style.visibility = "hidden";
+	document.getElementById("success").style.width =  "0px";
+	document.getElementById("success").style.height =  "0px";
+	document.getElementById("success").innerHTML = ""; 
+}
+
+$('#passhash').on('input', function (evt) {
+	var value = evt.target.value;
+	console.log(document.getElementById("passhash").innerHTML);
+	$('#passhash').val(value);
+	document.getElementById("lengtext").innerHTML = value.length;
+})
+$('#passhash').on('change', function (evt) {
+	evt.preventDefault();
+	var value = evt.target.value;
+	$('#passhash').val(value);
+})
 
 
 function htmlEncode(str){
@@ -78,6 +138,7 @@ function htmlEncode(str){
   });
 }
 function Afunctionthatdosomething(nu,isSym,isNumber,isMajes){
+  console.log("FIX ERREUR");
   let str = "";
   let breaker = 0;
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
